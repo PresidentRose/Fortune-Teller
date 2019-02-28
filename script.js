29b9bea95e73ed83f8867489020ce94bf01c23d1
PresidentRose <43241735+PresidentRose@users.noreply.github.com>
2019-02-28 09:49:00 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+$(".submit").click(function() {
+    var age= parseInt($(".age").val());
+    var age2= age-5;
+});
+
+$(".submit").click(function() {
+    var movie= parseInt($(".movie").val());
+    var solution= movie*1621;
+   $(".span2").text(solution);
+});
+
+
+$(".submit").click(function() {
+    var name=$(".name").val();
+    $(".span1").text(name);
+});
+
+
+
